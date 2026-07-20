969f1dacbe3abd19b9ef67f0ee822da106fd616d
neil314159 <89260616+neil314159@users.noreply.github.com>
2026-07-20 17:00:15 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "hitlog-v8";
+const CACHE = "hitlog-v9";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./styles.css", "./icon-192.png", "./icon-512.png", "./icon-180.png"];
 
 self.addEventListener("install", e => {
